f5cf486d466c93b2403c296bb87366c4f38498c6
Lorenzo Gonzalez <info@enzogonzalez.es>
2024-09-18 18:37:09 +0200

    style: add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { SearchBar } from "./components/SearchBar";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
 function App() {
   return (
