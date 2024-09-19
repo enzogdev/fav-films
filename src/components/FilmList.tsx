486d403b50a2e8b38a2f3a224c829e0944651e07
Lorenzo G <info@enzogonzalez.es>
2024-09-19 13:25:05 +0200

    feat: functionality of manage favorites

--- src/components/FilmList.tsx
+++ src/components/FilmList.tsx
@@ -1,22 +1,40 @@
-import { useEffect, useState } from "react";
-import { FilmCard } from "../components/FilmCard"; // Ajusta la ruta según la estructura de tu proyecto
+import { useDispatch, useSelector } from "react-redux";
+import { addFavorite, removeFavorite } from "../slices/favoriteSlice";
+import { RootState } from "../store/store";
+import { FilmCard } from "./FilmCard";
 import { useFetchFilms } from "../hooks/useFetchFilms";
-import { Film } from "../types/types";
-
-const handleFavoriteToggle = (imdbID: string) => {
-  console.log(`Toggling favorite for: ${imdbID}`);
-};
 
 export const FilmList = () => {
   const { films, error } = useFetchFilms();
+  const dispatch = useDispatch();
+  const favoriteFilms = useSelector(
+    (state: RootState) => state.favorites.favoriteFilms
+  );
 
-  useEffect(() => {
-    console.log(films);
-  }, [films]);
+  const handleFavoriteToggle = (imdbID: string) => {
+    const isFavorite = favoriteFilms.some(
+      (film: { imdbID: string }) => film.imdbID === imdbID
+    );
+    const film = films.find((film) => film.imdbID === imdbID);
+
+    if (film) {
+      if (isFavorite) {
+        dispatch(removeFavorite(imdbID));
+      } else {
+        dispatch(addFavorite(film));
+      }
+    }
+  };
+
+  // Verifica si una película está en favoritos
+  const isFilmFavorite = (imdbID: string) => {
+    return favoriteFilms.some((film) => film.imdbID === imdbID);
+  };
 
   if (error) {
     return <div>Error: {error}</div>;
   }
+
   return (
     <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
       {films.map((film) => (
@@ -26,8 +44,8 @@ export const FilmList = () => {
           title={film.Title}
           year={film.Year}
           poster={film.Poster}
-          onFavoriteToggle={handleFavoriteToggle}
-          isFavorite={false}
+          onFavoriteToggle={handleFavoriteToggle} // Pasar la función para togglear favoritos
+          isFavorite={isFilmFavorite(film.imdbID)} // Pasar si es favorito o no
         />
       ))}
     </div>
